52559774a757815a5ea34924bc89916834694887
Oscar <o.aguayo02@ufromail.cl>
2020-11-09 17:42:56 -0300

    se agregan datos a la rama develop

--- src/Lanzador.java
+++ src/Lanzador.java
@@ -1,5 +1,7 @@
 public class Lanzador {
     public static void main(String[] args) {
-
+      Banco banco= new Banco();
+      int[]credito={1230000,25};
+      banco.mostrarInformacionCredito(credito);
     }
 }
